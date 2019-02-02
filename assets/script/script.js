f10d8ea4ba2f2b93117816621832767491735073
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-02-02 11:15:22 -0600

    Add local storage

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,6 +10,8 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// Image address: https://www.lifelinkiii.com/wp-content/uploads/2018/10/Matt_Smiles.jpg
+
 $(".get-music").click(function (event) {
     event.preventDefault();
 
@@ -30,6 +32,7 @@ $(".get-music").click(function (event) {
 
     console.log(facePlusURL);
     console.log(userName);
+    localStorage.setItem("userName", userName);
 
     // Ajax call here -----------------------------------------------------------------------------------------
     $.ajax({
@@ -46,81 +49,82 @@ $(".get-music").click(function (event) {
                 Age: ageResponse,
                 gender: genderResponse,
                 link: faceLink,
-            })
+            });
+            localStorage.setItem("API-age", ageResponse);
         });
     $("form").trigger("reset")
 });
 
 //only 1995 from now on
 //create variables holding the top 10 songs
-var song1 = { 
+var song1 = {
     track: "Gangsta's Paradise",
     artist: "Coolio featuring L.V.",
     album_url: "../images/Gangsta's-Paradise.jpg",
     track_url: "https://www.youtube.com/watch?v=fPO76Jlnz6c"
 };
 
-var song2 = { 
+var song2 = {
     track: "Waterfalls",
     artist: "TLC",
     album_url: "../images/Crazy-Sexy-Cool.png",
     track_url: "https://www.youtube.com/watch?v=8WEtxJ4-sh4"
 };
 
-var song3 = { 
+var song3 = {
     track: "Creep",
     artist: "TLC",
     album_url: "../images/Crazy-Sexy-Cool.png",
-    track_url: "https://www.youtube.com/watch?v=LlZydtG3xqI"     
+    track_url: "https://www.youtube.com/watch?v=LlZydtG3xqI"
 };
 
-var song4 = { 
+var song4 = {
     track: "Kiss from a Rose",
     artist: "Seal",
     album_url: "../images/Kiss-From-A-Rose.jpg",
-    track_url: "https://www.youtube.com/watch?v=AMD2TwRvuoU"        
+    track_url: "https://www.youtube.com/watch?v=AMD2TwRvuoU"
 };
 
-var song5 = { 
+var song5 = {
     track: "On Bended Knee",
     artist: "Boyz II Men",
     album_url: "../images/On-Bended_knee.jpg",
-    track_url: "https://www.youtube.com/watch?v=jSUSFow70no"        
+    track_url: "https://www.youtube.com/watch?v=jSUSFow70no"
 };
 
-var song6 = { 
+var song6 = {
     track: "Another Night",
     artist: "Real McCoy",
     album_url: "../images/Another-Night.jpg",
-    track_url: "https://www.youtube.com/watch?v=Pav2f4b-1ZE"        
+    track_url: "https://www.youtube.com/watch?v=Pav2f4b-1ZE"
 };
 
-var song7 = { 
+var song7 = {
     track: "Fantasy",
     artist: "Mariah Carey",
     album_url: "../images/Fantasy.png",
-    track_url: "https://www.youtube.com/watch?v=qq09UkPRdFY"        
+    track_url: "https://www.youtube.com/watch?v=qq09UkPRdFY"
 };
 
-var song8 = { 
+var song8 = {
     track: "Take a Bow",
     artist: "Madonna",
     album_url: "../images/Take-a-Bow-Madonna.png",
-    track_url: "https://www.youtube.com/watch?v=XDeiovnCv1o"        
+    track_url: "https://www.youtube.com/watch?v=XDeiovnCv1o"
 };
 
-var song9 = { 
+var song9 = {
     track: "Don't Take It Personal (Just One of Dem Days)",
     artist: "Monica",
     album_url: "../images/Don't-Take-It-Personal-(Just-One-of-Dem-Days).jpg",
-    track_url: "https://www.youtube.com/watch?v=asXau88O5Is"        
+    track_url: "https://www.youtube.com/watch?v=asXau88O5Is"
 };
 
-var song10 = { 
+var song10 = {
     track: "This Is How We Do It",
     artist: "Montell Jordan",
     album_url: "../images/This-is-how-we-do-it.png",
-    track_url: "https://www.youtube.com/watch?v=0hiUuL5uTKc"        
+    track_url: "https://www.youtube.com/watch?v=0hiUuL5uTKc"
 };
 
 //set the database to include the songs
@@ -135,4 +139,4 @@ database.ref().set({
     song8: song8,
     song9: song9,
     song10: song10
-  });
+});
